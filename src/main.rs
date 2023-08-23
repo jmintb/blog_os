df5ea9f7016974d58173ffffdcc7081e0f354e05
Jessie Chatham Spencer <jessie@teainspace.com>
2023-08-23 13:59:23 +0200

    Complete chapters 1 - 3

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello WOrld{}", "!");
+    panic!("Help I am panicing");
+    loop {}
+}
